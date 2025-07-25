b50b78be712fe61b27d6b4949f4a9cbf264841af
agbibrightlucien <agbibrightlucienjnr@gmail.com>
2025-07-25 00:12:08 +0000

    feat: Add filtering, sorting, and pagination to user management and enhance CustomersPage with loading indicator

--- backend/primepre/settings.py
+++ backend/primepre/settings.py
@@ -161,6 +161,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
